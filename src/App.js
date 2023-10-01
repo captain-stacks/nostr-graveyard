fb977b45382a45c79b889eb4b1c1f17aba8fc86c
captain-stacks <joe@nostr.com>
2023-09-30 22:09:08 -0500

    checkpoint

--- src/App.js
+++ src/App.js
@@ -7,14 +7,31 @@ import {
 import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
+import Box from '@mui/material/Box'
+import LinearProgress from '@mui/material/LinearProgress'
 
 const pool = new SimplePool()
 window.pool = pool
 window.nip19 = nip19
 window.nip04 = nip04
+window.pool = pool
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/:npub?" element={<Page/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+function Page() {
+  const { npub } = useParams()
   const [pubkey, setPubkey] = useState()
+  const [profile, setProfile] = useState({})
+  const [followCount, setFollowCount] = useState(0)
+  const [progress, setProgress] = useState(0)
   const [contacts, setContacts] = useState([])
   const [relays, setRelays] = useState([
     'wss://relay.nostr.band',
@@ -24,32 +41,36 @@ function App() {
     'wss://nostr21.com/',
     'wss://nostr-pub.wellorder.net',
     'wss://offchain.pub',
-    'wss://relay.current.fyi',
     'wss://nostr.shroomslab.net',
     'wss://relayable.org',
     'wss://nostr.thank.eu'
   ].map(r => [r, {read: true, write: true}]))
 
+  window.setProgress = setProgress
+
   useEffect(() => {
-    let pubkey = localStorage.getItem('pubkey')
+    let pubkey = npub ? nip19.decode(npub).data : localStorage.getItem('pubkey')
     if (pubkey) {
       setPubkey(pubkey)
     } else {
       setTimeout(() => {
+        if (!window.nostr) {
+          alert('no nostr')
+          return
+        }
         window.nostr.getPublicKey()
           .then(pubkey => {
             localStorage.setItem('pubkey', pubkey)
             setPubkey(pubkey)
-        })
+        }).catch(e => alert('couldnt get pubkey'))
       }, 200)
     }
-  }, [])
+  }, [npub])
 
   useEffect(() => {
     if (pubkey) {
       (async () => {
-        await profile()
-        loadData()
+        await findProfile()
       })()
     }
   }, [pubkey])
@@ -66,74 +87,38 @@ function App() {
     return relays.map(r => r[0])
   }
 
-  window.getWriteRelays = getWriteRelays
-  window.getReadRelays = getReadRelays
   window.getAllRelays = () => relays.map(r => r[0])
   
-  async function profile() {
-    setContacts([])
-    let filter = {
-      kinds: [3],
+  async function findProfile() {
+    let events = await pool.list(getAllRelays(), [{
+      kinds: [0, 3],
       authors: [pubkey]
-    }
-    let events = await pool.list(getReadRelays(), [filter])
-    events.sort((a, b) => b.created_at - a.created_at)
-    let follows = events[0].tags.filter(t => t[0] === 'p').map(t => t[1])
-    follows.push(pubkey)
-    
-    filter = {
-      kinds: [0],
-      authors: follows
-    }
-    window.myFollows = {}
-    for (let f of follows) {
-      window.myFollows[f] = {
-        followedBy: [],
-        mutedBy: [],
-        reportedBy: []
-      }
-    }
-    events = await pool.list(getReadRelays(), [filter])
-    let contacts = []
-    let profiles = {}
-    events.forEach(e => {
-      try {
-        let c = JSON.parse(e.content)
-        c.npub = nip19.npubEncode(e.pubkey)
-        c.name = c.displayName || c.name
-        c.distrust = new Set()
-        c.followers = 0
-        contacts.push(c)
-        profiles[e.pubkey] = c
-      } catch (e) {
-        console.log('e', e)
-      }
-    })
-    setContacts(contacts)
-    window.events = events
-    window.profiles = profiles
+    }])
+    let profile = events.filter(e => e.kind === 0)
+    profile.sort((a, b) => b.created_at - a.created_at)
+    profile = profile[0]
+    let follows = events.filter(e => e.kind === 3)
+    follows.sort((a, b) => b.created_at - a.created_at)
+    follows = follows[0]
+    follows = follows.tags.filter(t => t[0] === 'p').map(t => t[1])
+    setContacts(follows)
+    setFollowCount(follows.length)
+    setProfile(JSON.parse(profile.content))
   }
 
-  async function follows(users) {
-    let filter = {
-      kinds: [3],
-      authors: users
-    }
-    let prefollows = await pool.list(getReadRelays(), [filter])
-    let follows = []
-    prefollows.forEach( (event) => {
-      var array = []
-      array.push(event["tags"])
-      var l = [event.pubkey]
-      array[0].forEach(item => l.push(item[1]))
-      follows.push(l)
-      l.forEach( (p) => {
-        if (p in window.myFollows) {
-          window.myFollows[p].followedBy.push(l[0])
+  async function loadData() {
+    const unfollow = []
+    for (const p of contacts) {
+        let count = (await pool.list(getReadRelays(), [{
+            authors: [p],
+            since: Math.floor(new Date('8/1/23') / 1000),
+            limit: 1
+        }])).length
+        console.log(nip19.npubEncode(p), count)
+        if (count == 0) {
+            unfollow.push(p)
         }
-      })
-    })
-    window.list_of_people_followed_by_my_followers = follows;
+    }
   }
 
   async function findRelays() {
@@ -156,116 +141,25 @@ function App() {
     setRelays(relays)
   }
 
-  window.follows = follows
-
-  window.pool = pool
-  window.getReadRelays = getReadRelays
-
-  async function loadDistrust() {
-    let users = Object.keys(window.myFollows)
-    let filters = [{
-      kinds: [10_000],
-      authors: users,
-      '#p': users
-    }, {
-      kinds: [30_000],
-      authors: users,
-      '#d': ['mute'],
-      '#p': users
-    }, {
-      kinds: [1984],
-      authors: users,
-      '#p': users
-    }, {
-      kinds: [3],
-      authors: users,
-      '#p': users
-    }]
-    for (let filter of filters) {
-      let events = await pool.list(getReadRelays(), [filter])
-      //console.log('users-', users)
-      console.log('events-', events)
-      events.forEach(e => {
-        if (e.kind === 1984) {
-          if (e.tags.find(t => t[0] === 'p')[1] in window.myFollows) {
-            window.myFollows[e.tags.find(t => t[0] === 'p')[1]].reportedBy.push(e.pubkey)
-          }
-        } else if (e.kind === 3) {
-          e.tags.filter(t => t[0] === 'p').forEach(t => {
-            window.myFollows[t[1]]?.followedBy.push(e.pubkey)
-          })
-        } else {
-          e.tags.filter(t => t[0] === 'p').forEach(t => {
-            window.myFollows[t[1]]?.mutedBy.push(e.pubkey)
-          })
-        }
-      })
-    }
-  }
-
-  async function loadData() {
-    await loadDistrust()
-
-    let myFollows = window.myFollows
-    let profiles = window.profiles
-
-    Object.keys(myFollows).forEach(k => {
-      let f = myFollows[k]
-      f.mutedBy.forEach(u => {
-        if (profiles[u]) profiles[k]?.distrust.add('muted by ' + profiles[u].name)
-      })
-      f.reportedBy.forEach(u => {
-        if (profiles[u]) profiles[k]?.distrust.add('reported by ' + profiles[u].name)
-      })
-      f.followedBy.forEach(u => {
-        if (profiles[u] && profiles[k]) profiles[k].followers++
-      })
-    })
-    setContacts(c => {
-      console.log('contacts', c)
-      console.log('filtered', [...c].filter(c => c.distrust.size))
-      c = [...c].filter(c => c.distrust.size)
-      c.forEach(c => c.score = Math.floor(100 * c.followers / (c.distrust.size + c.followers)))
-      c.sort((a, b) => a.score - b.score)
-      return c
-    })
-  }
-
-  function Page() {
-    const { npub } = useParams()
-    setPubkey(npub ? nip19.decode(npub).data : localStorage.getItem('pubkey'))
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="container">
-            <Link to='/'>Home</Link>
-            <p/>
-            {/* {relays.map(r => <div>{r[0]}</div>)} */}
-            {contacts.map(c => <div key={c.npub}>
-              <Link to={'/' + c.npub}>
-                <img src={c.picture} alt="" width={100}/>
-              </Link>
-              {' '} {c.name}
-              <br/> {c.score}%
-              {' '} trusted {c.followers} followers
-              {[...c.distrust].map(d => <div key={d}>
-                <small><small>{d}</small></small>
-              </div>)}
-              <p/>
-            </div>)}
-          </div>
-        </header>
-      </div>
-    )
-  }
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/:npub?" element={<Page/>}/>
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <img src={profile.picture} alt="" width={100}/>
+          {' '}{profile.name}{' follows '}{followCount}{' nostriches'}
+          <p/>
+          {/* <Link to='/'>Home</Link>{' '}
+          <Link to='/npub1jk9h2jsa8hjmtm9qlcca942473gnyhuynz5rmgve0dlu6hpeazxqc3lqz7'>Ser</Link> */}
+          <p/>
+          <button onClick={loadData}>Find inactive profiles</button>
+          <p/>
+          Finding inactive profiles...
+          <p/>
+          <LinearProgress sx={{height:50}} variant="determinate" value={progress} />
+
+        </div>
+      </header>
+    </div>
   )
 }
 
